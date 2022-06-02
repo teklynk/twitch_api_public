696c8433516b24847b4695b8a3d33dd332109f87
teklynk <ryanjones153@gmail.com>
2022-06-02 01:10:45 -0400

    added pagination option for after and before. Requires cursor value from the first request

--- public/getuserfollows.php
+++ public/getuserfollows.php
@@ -3,7 +3,23 @@ define('ALLOW_INC', true);
 
 require_once(__DIR__ . '/../config/config.php');
 
-$limit = $_GET['limit'];
+$limit = trim($_GET['limit']);
+$after = trim($_GET['after']);
+$before = trim($_GET['before']);
+
+if (!empty($after)) {
+    $afterVar = "&after=" . $after;
+    $beforeVar = "";
+} else {
+    $afterVar = "";
+}
+
+if (!empty($before)) {
+    $beforeVar = "&before=" . $before;
+    $afterVar = "";
+} else {
+    $beforeVar = "";
+}
 
 if ($limit > 100 ) {
     $limit = 100;
@@ -23,7 +39,7 @@ $userInfo = curl_exec($ch);
 $userResult = json_decode($userInfo, true);
 
 //Get user followers
-curl_setopt($ch, CURLOPT_URL, "https://api.twitch.tv/helix/users/follows?first=" . trim(strtolower($limit)) . "&to_id=" . $userResult['data'][0]['id'] . "");
+curl_setopt($ch, CURLOPT_URL, "https://api.twitch.tv/helix/users/follows?first=" . trim(strtolower($limit)) . "&to_id=" . $userResult['data'][0]['id'] . $afterVar . $beforeVar);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 $userResponse = curl_exec($ch);
