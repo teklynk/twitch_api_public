9ffe091efd2f978c1b59ab36b3d6fdc8e5ab35b3
teklynk <ryanjones153@gmail.com>
2022-09-20 21:41:07 -0400

    fixed issues with updating auth file

--- config/config.php
+++ config/config.php
@@ -3,14 +3,14 @@ $authFile = __DIR__ . '/.auth';
 
 $authFileModDate = date('Y-m-d', filemtime($authFile));
 $date_now = date('Y-m-d');
+$authtoken = '';
 
 // Note: The files are hidden from the file system
-define('CLIENT_ID', file_get_contents(__DIR__ . '/.client'));
-define('CLIENT_SECRET', file_get_contents(__DIR__ . '/.secret'));
+define('CLIENT_ID', trim(file_get_contents(__DIR__ . '/.client')));
+define('CLIENT_SECRET', trim(file_get_contents(__DIR__ . '/.secret')));
 
 // Refresh oAuth Token automatically
 if (strtotime($date_now) > strtotime($authFileModDate) || file_get_contents($authFile) == '') {
-
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "https://id.twitch.tv/oauth2/token?client_id=" . CLIENT_ID . "&client_secret=" . CLIENT_SECRET . "&grant_type=client_credentials");
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -30,10 +30,10 @@ if (strtotime($date_now) > strtotime($authFileModDate) || file_get_contents($aut
         // Write auth token to .auth file
         file_put_contents($authFile, $writeauthtoken);
     }
-
 }
 
 $authtoken = file_get_contents($authFile);
 
-define('AUTH_TOKEN', $authtoken);
+define('AUTH_TOKEN', trim($authtoken));
+
 ?>
