0b13a3c9450cd74ba7e5801ded200128482c4b99
teklynk <teklynk@protonmail.com>
2026-01-23 17:45:34 -0500

    added logic to check if running inside docker

--- public/getuserclips.php
+++ public/getuserclips.php
@@ -36,9 +36,12 @@ $cached = null;
 $mem = null;
 if (class_exists('Memcached')) {
     $mem = new Memcached();
-    // If using Docker
-    //$mem->addServer("memcached", 11211);
-    $mem->addServer("127.0.0.1", 11211);
+    // Check if running in Docker (memcached hostname resolves)
+    if (gethostbyname('memcached') !== 'memcached') {
+        $mem->addServer("memcached", 11211);
+    } else {
+        $mem->addServer("127.0.0.1", 11211);
+    }
     $cacheKey = 'twitch_clips_' . md5(json_encode([$channel, $id, $limit, $random, $start_date, $end_date, $prefer_featured, $creator_name, $shuffle, ($random == 'true' ? ($_GET['count'] ?? '1') : '')]));
     $cached = $mem->get($cacheKey);
 }
