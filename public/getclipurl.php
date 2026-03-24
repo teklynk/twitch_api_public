a2de42d0998d84821bf7e32f485b470005c01199
teklynk <teklynk@protonmail.com>
2026-03-23 20:54:10 -0400

    added memcached to getclipurl to speed up clip lookup time and save a call out to Twitch api

--- public/getclipurl.php
+++ public/getclipurl.php
@@ -17,62 +17,89 @@ $url = isset($_GET['url']) ? $_GET['url'] : 'true';
 // https://example.com/getclipurl.php?id=LaconicCulturedPieMingLee-qxlzFZb89ZlEgdzP
 if ($id) {
 
-    // Get user clips
-    $response = $client->request('GET', "https://api.twitch.tv/helix/clips?id=" . $id, [
-        'headers' => $headers
-    ]);
+    $clip_url = null;
 
-    $userStatus = $response->getStatusCode();
+    $cacheTTL = 3600; // 1 hour
 
-    if ($userStatus == 200) {
-        // all clips data
-        $userData = json_decode($response->getBody(), true);
-
-        foreach ($userData['data'] as $data) {
-
-            $body = json_encode([
-                [
-                    "operationName" => "VideoAccessToken_Clip",
-                    "variables" => ["platform" => "web", "slug" => $data['id']],
-                    "extensions" => ["persistedQuery" => ["version" => 1, "sha256Hash" => TWITCH_SHA256HASH]]
-                ]
-            ]);
-
-            $response = $client->request('POST', TWITCH_GRAPHQL_URL, [
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                    'Client-ID' => TWITCH_CLIENT_ID
-                ],
-                'body' => $body
-            ]);
-
-            $clips_array = json_decode($response->getBody(), true);
-
-            $clips_signature = $clips_array[0]['data']['clip']['playbackAccessToken']['signature'];
-            $clips_token = urlencode($clips_array[0]['data']['clip']['playbackAccessToken']['value']);
-            $clips_video_source = $clips_array[0]['data']['clip']['videoQualities'][0]['sourceURL'];
-
-            $clip_url = $clips_video_source . '?sig=' . $clips_signature . '&token=' . $clips_token;
-            break;
+    $mem = null;
+    if (class_exists('Memcached')) {
+        $mem = new Memcached();
+        if (gethostbyname('memcached') !== 'memcached') {
+            $mem->addServer("memcached", 11211);
+        } else {
+            $mem->addServer("127.0.0.1", 11211);
         }
+        $cacheKey = 'twitch_clip_url_' . md5($id);
+        $clip_url = $mem->get($cacheKey);
+    }
 
-        if (isset($clip_url) && $clip_url) {
-            if ($url == 'false') {
-                // render as a video source file
-                $video = $client->request('GET', $clip_url, ['stream' => true]);
-                header('Content-Type: video/mp4');
-                header('Content-Length: ' . $video->getHeaderLine('Content-Length'));
+    if (!$clip_url) {
+        try {
+            // Get user clips
+            $response = $client->request('GET', "https://api.twitch.tv/helix/clips?id=" . $id, [
+                'headers' => $headers
+            ]);
 
-                $body = $video->getBody();
-                while (!$body->eof()) {
-                    echo $body->read(8192);
+            $userStatus = $response->getStatusCode();
+
+            if ($userStatus == 200) {
+                // all clips data
+                $userData = json_decode($response->getBody(), true);
+
+                foreach ($userData['data'] as $data) {
+
+                    $body = json_encode([
+                        [
+                            "operationName" => "VideoAccessToken_Clip",
+                            "variables" => ["platform" => "web", "slug" => $data['id']],
+                            "extensions" => ["persistedQuery" => ["version" => 1, "sha256Hash" => TWITCH_SHA256HASH]]
+                        ]
+                    ]);
+
+                    $response = $client->request('POST', TWITCH_GRAPHQL_URL, [
+                        'headers' => [
+                            'Content-Type' => 'application/json',
+                            'Client-ID' => TWITCH_CLIENT_ID
+                        ],
+                        'body' => $body
+                    ]);
+
+                    $clips_array = json_decode($response->getBody(), true);
+
+                    $clips_signature = $clips_array[0]['data']['clip']['playbackAccessToken']['signature'];
+                    $clips_token = urlencode($clips_array[0]['data']['clip']['playbackAccessToken']['value']);
+                    $clips_video_source = $clips_array[0]['data']['clip']['videoQualities'][0]['sourceURL'];
+
+                    $clip_url = $clips_video_source . '?sig=' . $clips_signature . '&token=' . $clips_token;
+                    break;
                 }
-                exit();
-            } else {
-                // redirect to video clip url
-                header('Location: ' . $clip_url, true, 302);
-                exit();
+
+                if ($mem && isset($clip_url) && $clip_url) {
+                    $mem->set($cacheKey, $clip_url, $cacheTTL);
+                }
             }
+        } catch (\Exception $e) {
+            // Log error or ignore
+        }
+    }
+
+    if (isset($clip_url) && $clip_url) {
+        if ($url == 'false') {
+            // render as a video source file
+            $video = $client->request('GET', $clip_url, ['stream' => true]);
+            header('Content-Type: video/mp4');
+            header('Content-Length: ' . $video->getHeaderLine('Content-Length'));
+
+            $body = $video->getBody();
+            while (!$body->eof()) {
+                // Chunk size: 8192 = 8 Kilobytes
+                echo $body->read(8192);
+            }
+            exit();
+        } else {
+            // redirect to Twitch video clip url
+            header('Location: ' . $clip_url, true, 302);
+            exit();
         }
     }
 
